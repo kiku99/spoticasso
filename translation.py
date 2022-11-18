fec40e4807a7873267bc1ecdd0dc9b34b214b512
dvpaa <kgo0926@naver.com>
2022-11-19 01:04:47 +0900

    create translation.py

---
+++ translation.py
@@ -0,0 +1,14 @@
+from googletrans import Translator
+
+
+def lyrics_to_english(lyrics: str):
+    translater = Translator()
+    l = ""
+    for s in translater.translate(lyrics, dest='en').text:
+        l += s
+    return l
+
+
+if __name__ == "__main__":
+    import Lyrics
+    print(lyrics_to_english(Lyrics.get_lyrics(track_id="6CV6j2xz54thzlrWML3kAW", api_key="Mn0TrmZhj9QRsQHm4iBnguUm7VSKpe67")))
